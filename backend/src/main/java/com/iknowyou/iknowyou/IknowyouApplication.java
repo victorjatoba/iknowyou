bffb9d647ea30e2b94335915f2b1b0bad23f7866
Luigi <scarminio@gmail.com>
2019-03-24 10:34:43 -0300

    First backend commit

---
+++ backend/src/main/java/com/iknowyou/iknowyou/IknowyouApplication.java
@@ -0,0 +1,13 @@
+package com.iknowyou.iknowyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IknowyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IknowyouApplication.class, args);
+	}
+
+}

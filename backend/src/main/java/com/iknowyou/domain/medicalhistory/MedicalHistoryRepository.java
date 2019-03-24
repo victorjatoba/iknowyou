892ffab12e396ea526c1276cede8c03910cdb80a
Luigi <scarminio@gmail.com>
2019-03-24 14:16:18 -0300

    #3 Implements MedicalHistoryRepository

---
+++ backend/src/main/java/com/iknowyou/domain/medicalhistory/MedicalHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.iknowyou.domain.medicalhistory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MedicalHistoryRepository extends JpaRepository<MedicalHistoryRepository, Integer> {
+
+}
